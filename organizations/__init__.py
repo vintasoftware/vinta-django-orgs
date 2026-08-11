2d408207491de7c9ab62f4d7ab0901aacd95a852
Hugo bessa <hugo@vinta.com.br>
2026-08-11 15:13:40 -0300

    Update project metadata: change title, version, and copyright information

--- organizations/__init__.py
+++ organizations/__init__.py
@@ -1,8 +1,8 @@
-__title__ = 'Django Model Organizations'
-__version__ = '0.0.3'
+__title__ = 'Vinta Django Orgs'
+__version__ = '0.1.0'
 __author__ = 'Hugo Bessa'
 __license__ = 'MIT License'
-__copyright__ = 'Copyright 017 Hugo Bessa'
+__copyright__ = 'Copyright Vinta Software'
 
 # Version synonym
 VERSION = __version__
